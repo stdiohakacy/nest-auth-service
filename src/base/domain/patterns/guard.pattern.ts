614f91194203907781ea5229c9746ac3041e59bb
Cornal Nguyen <duy.nguyend.tpv@one-line.com>
2025-05-16 23:09:34 +0700

    feat: done base

--- src/base/domain/patterns/guard.pattern.ts
+++ src/base/domain/patterns/guard.pattern.ts
@@ -1,113 +1,55 @@
-import { Result } from './result.pattern';
-
-export type GuardResponse = string;
-
-export interface GuardArgumentInterface<T> {
-  argument: T;
-  argumentName: string;
-}
-
-export type GuardArgumentCollection<T = unknown> = GuardArgumentInterface<T>[];
-
 export class Guard {
-  public static combine(
-    guardResults: Result<GuardResponse>[],
-  ): Result<GuardResponse> {
-    for (const result of guardResults) {
-      if (result.isFailure) return result;
+  /**
+   * Checks if value is empty. Accepts strings, numbers, booleans, objects and arrays.
+   */
+  static isEmpty(value: unknown): boolean {
+    if (typeof value === 'number' || typeof value === 'boolean') {
+      return false;
     }
-    return Result.ok();
-  }
-
-  public static greaterThan(
-    minValue: number,
-    actualValue: number,
-  ): Result<GuardResponse> {
-    return actualValue > minValue
-      ? Result.ok()
-      : Result.fail(
-          `Number given {${actualValue}} is not greater than {${minValue}}`,
-        );
-  }
-
-  public static againstAtLeast(
-    numChars: number,
-    text: string,
-  ): Result<GuardResponse> {
-    return text.length >= numChars
-      ? Result.ok()
-      : Result.fail(`Text is not at least ${numChars} chars.`);
-  }
-
-  public static againstAtMost(
-    numChars: number,
-    text: string,
-  ): Result<GuardResponse> {
-    return text.length <= numChars
-      ? Result.ok()
-      : Result.fail(`Text is greater than ${numChars} chars.`);
-  }
-
-  public static againstNullOrUndefined<T>(
-    argument: T | null | undefined,
-    argumentName: string,
-  ): Result<GuardResponse> {
-    if (argument === null || argument === undefined) {
-      return Result.fail(`${argumentName} is null or undefined`);
+    if (typeof value === 'undefined' || value === null) {
+      return true;
     }
-    return Result.ok();
-  }
-
-  public static againstNullOrUndefinedBulk<T>(
-    args: GuardArgumentCollection<T>,
-  ): Result<GuardResponse> {
-    for (const arg of args) {
-      const result = this.againstNullOrUndefined(
-        arg.argument,
-        arg.argumentName,
-      );
-      if (result.isFailure) return result;
+    if (value instanceof Date) {
+      return false;
     }
-    return Result.ok();
-  }
-
-  public static isOneOf<T>(
-    value: T,
-    validValues: T[],
-    argumentName: string,
-  ): Result<GuardResponse> {
-    const isValid = validValues.includes(value);
-    return isValid
-      ? Result.ok()
-      : Result.fail(
-          `${argumentName} isn't one of the allowed values: ${JSON.stringify(validValues)}. Got "${value}".`,
-        );
-  }
-
-  public static inRange(
-    num: number,
-    min: number,
-    max: number,
-    argumentName: string,
-  ): Result<GuardResponse> {
-    const isInRange = num >= min && num <= max;
-    return isInRange
-      ? Result.ok()
-      : Result.fail(`${argumentName} is not within range ${min} to ${max}.`);
-  }
-
-  public static allInRange(
-    numbers: number[],
-    min: number,
-    max: number,
-    argumentName: string,
-  ): Result<GuardResponse> {
-    for (const num of numbers) {
-      const result = this.inRange(num, min, max, argumentName);
-      if (result.isFailure) {
-        return Result.fail(`${argumentName} is not within the range.`);
+    if (value instanceof Object && !Object.keys(value).length) {
+      return true;
+    }
+    if (Array.isArray(value)) {
+      if (value.length === 0) {
+        return true;
+      }
+      if (value.every((item) => Guard.isEmpty(item))) {
+        return true;
       }
     }
-    return Result.ok();
+    if (value === '') {
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * Checks length range of a provided number/string/array
+   */
+  static lengthIsBetween(
+    value: number | string | Array<unknown>,
+    min: number,
+    max: number,
+  ): boolean {
+    if (Guard.isEmpty(value)) {
+      throw new Error(
+        'Cannot check length of a value. Provided value is empty',
+      );
+    }
+    const valueLength =
+      typeof value === 'number'
+        ? Number(value).toString().length
+        : value.length;
+    if (valueLength >= min && valueLength <= max) {
+      return true;
+    }
+    return false;
   }
 }
