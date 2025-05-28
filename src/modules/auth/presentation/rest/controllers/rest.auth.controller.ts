06605316106dd0694b3ca020968faa6b55630e43
Cornal Nguyen <duy.nguyend.tpv@one-line.com>
2025-05-28 17:10:53 +0700

    feat: done api auth register

--- src/modules/auth/presentation/rest/controllers/rest.auth.controller.ts
+++ src/modules/auth/presentation/rest/controllers/rest.auth.controller.ts
@@ -2,13 +2,14 @@ import { RegisterCommand } from '@module/auth/application/commands/register/regi
 import { Body, Controller, Post } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { routesV1 } from 'src/configs/app.routes';
+import { RegisterRestRequestDto } from '../dtos/request/register.rest-request.dto';
 
 @Controller(routesV1.auth.root)
 export class RestAuthController {
   constructor(private readonly commandBus: CommandBus) {}
 
   @Post('/register')
-  async register(@Body() body: any): Promise<any> {
+  async register(@Body() body: RegisterRestRequestDto): Promise<any> {
     await this.commandBus.execute(new RegisterCommand(body));
   }
 }
---
+++ src/modules/auth/presentation/rest/dtos/request/register.rest-request.dto.ts
@@ -0,0 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class RegisterRestRequestDto {
+  @ApiProperty({
+    example: 'john@gmail.com',
+    description: 'User email address',
+  })
+  @MaxLength(320)
+  @MinLength(5)
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    example: 'John Doe',
+    description: 'User full name',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    example: 'Abc@1234',
+    description: 'User password',
+  })
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+    },
+  )
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
